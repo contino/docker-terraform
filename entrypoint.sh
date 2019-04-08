4082d6626b3b0fcb8f70ce1b496dc4e793888f21
Lucas Teligioridis <lucas.teligioridis@gmail.com>
2019-04-08 17:29:48 +0100

    Need to change home location

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,9 @@ GROUP_ID=${TERRAFORM_GID:-$(stat -c %g "$(pwd)")}
 USER_NAME=${TERRAFORM_USER:-terraform}
 GROUP_NAME=${TERRAFORM_GROUP:-terraform}
 
+# change to new home directory
+export HOME=/home/${USER_NAME}
+
 # only create new user if we haven't docker remapped
 # https://docs.docker.com/engine/security/userns-remap/
 if [ "${USER_ID}" == "0" ]; then
@@ -15,9 +18,6 @@ else
   # add our custom group and user
   addgroup -S -g "${GROUP_ID}" "${GROUP_NAME}"
   adduser -S -s /bin/bash -u "${USER_ID}" -G "${GROUP_NAME}" "${USER_NAME}"
-
-  # change to new home directory
-  export HOME=/home/${USER_NAME}
 
   # run commands with new UID and GID
   exec gosu "${USER_NAME}" terraform "$@"
